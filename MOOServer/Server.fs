a6abb2c968056e4ebe478dc41c5d41c28135478f
Ville Nurmi <ville.v.nurmi@gmail.com>
2013-03-25 20:05:40 +0200

    Added a simple server that keeps adding planets as you press enter

---
+++ MOOServer/Server.fs
@@ -0,0 +1,32 @@
+﻿module MOO.Server
+
+open MOO.State
+open MOO.Types
+open System
+
+let printPlanet p =
+    printfn "%s, population %i/%i, orbit %i" p.name p.population p.maxPopulation p.orbit
+let printState =
+    state {
+        let! planets = getPlanets
+        printfn "\n%i planets" planets.Count
+        Map.iter (fun id p -> printPlanet p) planets
+    }
+let rec uiLoop () =
+    state {
+        do! printState
+        let input = Console.ReadLine()
+        if input <> "q" then
+            let! planets = getPlanets
+            let n = planets.Count + 1
+            let planet = {
+                id = 0
+                name = sprintf "Planet %i" n
+                maxPopulation = 99
+                population = 42
+                orbit = n
+            }
+            do! addPlanet planet
+            do! uiLoop ()
+    }
+let _, finalState = run (uiLoop ()) initialState
