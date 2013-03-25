a470907b456c56840f67b2ba56b38e33ac0366d5
Ville Nurmi <ville.v.nurmi@gmail.com>
2013-03-25 21:24:00 +0200

    The server opens a service host both over HTTP and over a named pipe.
    Note: To create an HTTP end point, you probably need to run the server with Windows admin rights. Otherwise you'll get an exception at startup.

--- MOOServer/Server.fs
+++ MOOServer/Server.fs
@@ -1,8 +1,11 @@
 ﻿module MOO.Server
 
+open MOO.Service
 open MOO.State
 open MOO.Types
 open System
+open System.ServiceModel
+open System.ServiceModel.Description
 
 let printPlanet p =
     printfn "%s, population %i/%i, orbit %i" p.name p.population p.maxPopulation p.orbit
@@ -29,4 +32,18 @@ let rec uiLoop () =
             do! addPlanet planet
             do! uiLoop ()
     }
-let _, finalState = run (uiLoop ()) initialState
+let main =
+    let httpBaseAddress = new Uri(Uri.UriSchemeHttp + "://localhost:8000/MOO")
+    let pipeBaseAddress = new Uri(Uri.UriSchemeNetPipe + "://localhost/MOO")
+    use host = new ServiceHost(typeof<MOOService>, httpBaseAddress, pipeBaseAddress)
+    try
+        host.AddServiceEndpoint(typeof<IMOOService>, new WSHttpBinding(), "MOOService") |> ignore
+        host.AddServiceEndpoint(typeof<IMOOService>, new NetNamedPipeBinding(), "MOOService") |> ignore
+        host.Description.Behaviors.Add <| new ServiceMetadataBehavior(HttpGetEnabled = true)
+        host.Open()
+        let _, finalState = run (uiLoop ()) initialState
+        host.Close()
+    with
+    | :? CommunicationException as ex ->
+        printfn "An exception occurred: %s" ex.Message
+        host.Abort()
