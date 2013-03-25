3d1ed65290fabcd64177215a209bcec92e93ccd3
Ville Nurmi <ville.v.nurmi@gmail.com>
2013-03-25 22:04:34 +0200

    Pressing space makes the client retrieve planets

--- MOOClient/Client.cs
+++ MOOClient/Client.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+using MOO.Client.MOOService;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,16 +16,30 @@ namespace MOO.Client
     {
         private GraphicsDeviceManager _graphics;
         private ContentManager _content;
+        private KeyboardState _oldKeyboard;
+        private MOOServiceClient _service;
+        private Planet[] _planets;
 
         public Client()
         {
             _graphics = new GraphicsDeviceManager(this);
             _content = new ContentManager(Services);
+            _service = new MOOServiceClient("NetNamedPipeBinding_IMOOService1");
+            _planets = new Planet[0];
         }
     
         protected override void Draw(GameTime gameTime)
         {
             _graphics.GraphicsDevice.Clear(Color.Red);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            var keyboard = Keyboard.GetState();
+            if (!_oldKeyboard.IsKeyDown(Keys.Space) && keyboard.IsKeyDown(Keys.Space))
+                _planets = _service.GetPlanets();
+            _oldKeyboard = keyboard;
         }
     }
 
