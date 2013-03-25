f5ff6e2c2c02935014d2bd32a0218fbf81b77711
Ville Nurmi <ville.v.nurmi@gmail.com>
2013-03-25 20:06:07 +0200

    Added a simple client that displays a red screen using XNA

---
+++ MOOClient/Client.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOO.Client
+{
+    public class Client : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private ContentManager _content;
+
+        public Client()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            _content = new ContentManager(Services);
+        }
+    
+        protected override void Draw(GameTime gameTime)
+        {
+            _graphics.GraphicsDevice.Clear(Color.Red);
+        }
+    }
+
+    public static class App
+    {
+        public static void Main(string[] args)
+        {
+            using (var game = new Client()) game.Run();
+        }
+    }
+}
