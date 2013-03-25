62519ed70e539ac641d8551918ebb816ab4ca5e2
Ville Nurmi <ville.v.nurmi@gmail.com>
2013-03-25 20:05:11 +0200

    Added project MOOService that defines the interface between the server and the clients

---
+++ MOOService/IMOOService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MOO.Service
+{
+    [ServiceContract]
+    public interface IMOOService
+    {
+        [OperationContract]
+        Planet[] GetPlanets();
+    }
+
+    [DataContract]
+    public class Planet
+    {
+        [DataMember]
+        public int ID { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public int Population { get; set; }
+
+        [DataMember]
+        public int MaxPopulation { get; set; }
+
+        [DataMember]
+        public int Orbit { get; set; }
+    }
+}
---
+++ MOOService/MOOService.svc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MOO.Service
+{
+    public class MOOService : IMOOService
+    {
+        public Planet[] GetPlanets()
+        {
+            return new[]
+            {
+                new Planet { ID = 42, Name = "Foo", MaxPopulation = 99, Population = 69, Orbit = 3 }
+            };
+        }
+    }
+}
