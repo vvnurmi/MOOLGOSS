b51a2ecfddb71a9626f6cdfe14098870a7d609f8
Ville Nurmi <ville.v.nurmi@gmail.com>
2013-03-26 12:42:58 +0200

    Replaced XNA by WPF as the GUI technology.
    Motivation: XNA Content Projects are very difficult to set up in Visual Studio 2012, which heavily cripples XNA.
    Note: WPF integration is difficult in Visual Studio 2012 Express for Web. Writing XAML in a designer seems pretty much out of the question. However, the client is just a small demo app, so this will not be a problem for the future.

--- MOOClient/Client.cs
+++ MOOClient/Client.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Input;
+﻿using MOO.Client.GUI;
 using MOO.Client.MOOService;
 using System;
 using System.Collections.Generic;
@@ -9,45 +7,37 @@ using System.ServiceModel;
 using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MOO.Client
 {
-    public class Client : Game
+    public class Client : Application
     {
-        private GraphicsDeviceManager _graphics;
-        private ContentManager _content;
-        private KeyboardState _oldKeyboard;
         private MOOServiceClient _service;
         private Planet[] _planets;
 
         public Client()
         {
-            _graphics = new GraphicsDeviceManager(this);
-            _content = new ContentManager(Services);
             _service = new MOOServiceClient("NetNamedPipeBinding_IMOOService1");
             _planets = new Planet[0];
         }
-    
-        protected override void Draw(GameTime gameTime)
-        {
-            _graphics.GraphicsDevice.Clear(Color.Red);
-        }
 
-        protected override void Update(GameTime gameTime)
-        {
-            base.Update(gameTime);
-            var keyboard = Keyboard.GetState();
-            if (!_oldKeyboard.IsKeyDown(Keys.Space) && keyboard.IsKeyDown(Keys.Space))
-                _planets = _service.GetPlanets();
-            _oldKeyboard = keyboard;
-        }
+        //protected override void Update(GameTime gameTime)
+        //{
+        //    base.Update(gameTime);
+        //    var keyboard = Keyboard.GetState();
+        //    if (!_oldKeyboard.IsKeyDown(Keys.Space) && keyboard.IsKeyDown(Keys.Space))
+        //        _planets = _service.GetPlanets();
+        //    _oldKeyboard = keyboard;
+        //}
     }
 
     public static class App
     {
+        [STAThread]
         public static void Main(string[] args)
         {
-            using (var game = new Client()) game.Run();
+            new Client().Run(new ClientWindow());
         }
     }
 }
---
+++ MOOClient/GUI/ClientWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MOO.Client.GUI
+{
+    public partial class ClientWindow : Window
+    {
+        public ClientWindow()
+        {
+            Background = Brushes.Black;
+            Content = new TextBlock(new Run("Hello")) { Foreground = Brushes.White };
+        }
+    }
+}
