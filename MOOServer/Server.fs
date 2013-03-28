ccb8b6a9078991706b39b2613035394a1f908c0b
Ville Nurmi <ville.v.nurmi@gmail.com>
2013-03-28 09:54:34 +0200

    Extracted the service implementation to its own module

--- MOOServer/Server.fs
+++ MOOServer/Server.fs
@@ -1,28 +1,9 @@
-﻿module MOO.Server
+﻿module MOO.Server.App
 
-open MOO.Service
+open MOO.Server.Service
 open MOO.State
 open MOO.Types
 open System
-open System.ServiceModel
-open System.ServiceModel.Description
-
-type ServiceState = {
-    mutable planets : Planet array
-}
-let serviceState = {
-    planets = [||]
-}
-let updateServiceState =
-    state {
-        let! planets = getPlanets
-        serviceState.planets <- Array.map snd <| Map.toArray planets
-    }
-
-[<ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)>]
-type MOOService() =
-    interface IMOOService with
-        member x.GetPlanets() = serviceState.planets
 
 let printPlanet p =
     printfn "%s, population %i/%i, orbit %i" p.name p.population p.maxPopulation p.orbit
@@ -51,17 +32,4 @@ let rec uiLoop () =
             do! uiLoop ()
     }
 let main =
-    let httpBaseAddress = new Uri(Uri.UriSchemeHttp + "://localhost:8000/MOO")
-    let pipeBaseAddress = new Uri(Uri.UriSchemeNetPipe + "://localhost/MOO")
-    use host = new ServiceHost(typeof<MOOService>, httpBaseAddress, pipeBaseAddress)
-    try
-        host.AddServiceEndpoint(typeof<IMOOService>, new WSDualHttpBinding(), "MOOService") |> ignore
-        host.AddServiceEndpoint(typeof<IMOOService>, new NetNamedPipeBinding(), "MOOService") |> ignore
-        host.Description.Behaviors.Add <| new ServiceMetadataBehavior(HttpGetEnabled = true)
-        host.Open()
-        let _, finalState = run (uiLoop ()) initialState
-        host.Close()
-    with
-    | :? CommunicationException as ex ->
-        printfn "An exception occurred: %s" ex.Message
-        host.Abort()
+    runWithService <| fun () -> run (uiLoop ()) initialState |> ignore
---
+++ MOOServer/Service.fs
@@ -0,0 +1,41 @@
+﻿module MOO.Server.Service
+
+open MOO.Service
+open MOO.State
+open MOO.Types
+open System
+open System.ServiceModel
+open System.ServiceModel.Description
+
+type ServiceState = {
+    mutable planets : Planet array
+}
+let serviceState = {
+    planets = [||]
+}
+let updateServiceState =
+    state {
+        let! planets = getPlanets
+        serviceState.planets <- Array.map snd <| Map.toArray planets
+    }
+
+[<ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)>]
+type MOOService() =
+    interface IMOOService with
+        member x.GetPlanets() = serviceState.planets
+
+let runWithService f =
+    let httpBaseAddress = new Uri(Uri.UriSchemeHttp + "://localhost:8000/MOO")
+    let pipeBaseAddress = new Uri(Uri.UriSchemeNetPipe + "://localhost/MOO")
+    use host = new ServiceHost(typeof<MOOService>, httpBaseAddress, pipeBaseAddress)
+    try
+        host.AddServiceEndpoint(typeof<IMOOService>, new WSDualHttpBinding(), "MOOService") |> ignore
+        host.AddServiceEndpoint(typeof<IMOOService>, new NetNamedPipeBinding(), "MOOService") |> ignore
+        host.Description.Behaviors.Add <| new ServiceMetadataBehavior(HttpGetEnabled = true)
+        host.Open()
+        f ()
+        host.Close()
+    with
+    | :? CommunicationException as ex ->
+        printfn "An exception occurred: %s" ex.Message
+        host.Abort()
