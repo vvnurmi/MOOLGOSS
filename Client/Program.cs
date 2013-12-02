ea3f7083d558dae88401ccb0ea94c3652efe3039
Ville Nurmi <ville.v.nurmi@gmail.com>
2013-12-02 18:45:15 +0200

    Added simple server and client programs that say hello

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +12,14 @@ namespace Client
     {
         public static void Main(string[] args)
         {
-            var client = new Client();
+            var request = WebRequest.Create("http://localhost:8080/moolgoss/");
+            using (var response = request.GetResponse())
+            {
+                var stream = new StreamReader(response.GetResponseStream());
+                Console.WriteLine(stream.ReadToEnd());
+            }
+            Console.WriteLine("Press Enter to finish");
+            Console.ReadLine();
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,39 @@ namespace Server
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            var server = new Server();
+            var listener = new HttpListener();
+            listener.Prefixes.Add("http://*:8080/moolgoss/");
+            if (!TryStart(listener)) return 1;
+            while (true)
+            {
+                var context = listener.GetContext();
+                Console.WriteLine("Contacted from {0}", context.Request.UserHostAddress);
+                var data = Encoding.UTF8.GetBytes("Hello there!");
+                context.Response.Close(data, true);
+            }
+        }
+
+        private static bool TryStart(HttpListener listener)
+        {
+            var prefixes = listener.Prefixes;
+            try
+            {
+                listener.Start();
+                return true;
+            }
+            catch (HttpListenerException ex)
+            {
+                if (ex.ErrorCode != 5) throw;
+                var username = Environment.GetEnvironmentVariable("USERNAME");
+                var userdomain = Environment.GetEnvironmentVariable("USERDOMAIN");
+                Console.WriteLine("Access limitation. You need to run this first:");
+                foreach (var prefix in prefixes)
+                    Console.WriteLine("  netsh http add urlacl url={0} user={1}\\{2} listen=yes",
+                        prefix, userdomain, username);
+                return false;
+            }
         }
     }
 }
