2fbd4f2c4c216a9f34cec35e4cc4d25574d4376e
Ville Nurmi <ville.v.nurmi@gmail.com>
2013-12-28 11:17:07 +0200

    Added Ship with basic moving capabilities

---
+++ Core/Ship.cs
@@ -0,0 +1,49 @@
+﻿using Axiom.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    [Serializable]
+    public class Ship : IEntity
+    {
+        [Prop]
+        public Vector3 Pos { get; private set; }
+
+        [Prop]
+        public Vector3 Front { get; private set; }
+
+        [Prop]
+        public Vector3 Up { get; private set; }
+
+        public Vector3 Right { get { return Front.Cross(Up).ToNormalized(); } }
+
+        public Ship(Vector3 pos, Vector3 front, Vector3 up)
+        {
+            Pos = pos;
+            Front = front;
+            Up = up;
+        }
+
+        public void Move(Vector3 delta)
+        {
+            Pos += delta;
+        }
+
+        public void Pitch(float degrees)
+        {
+            var rotation = Quaternion.FromAngleAxis(Utility.DegreesToRadians(degrees), Right);
+            Front = rotation * Front;
+            Up = rotation * Up;
+        }
+
+        public void Yaw(float degrees)
+        {
+            var rotation = Quaternion.FromAngleAxis(Utility.DegreesToRadians(degrees), Up);
+            Front = rotation * Front;
+        }
+    }
+}
---
+++ Tests/ShipTest.cs
@@ -0,0 +1,43 @@
+﻿using Axiom.Math;
+using Core;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ShipTest
+    {
+        private Ship _ship;
+
+        [SetUp]
+        public void Setup()
+        {
+            _ship = new Ship(new Vector3(2, 3, 4), Vector3.UnitX, Vector3.UnitY);
+        }
+
+        [Test]
+        public void TestCreation()
+        {
+            Assert.AreEqual(new Vector3(2, 3, 4), _ship.Pos);
+            Assert.AreEqual(new Vector3(1, 0, 0), _ship.Front);
+            Assert.AreEqual(new Vector3(0, 1, 0), _ship.Up);
+            Assert.AreEqual(new Vector3(0, 0, 1), _ship.Right);
+        }
+
+        [Test]
+        public void TestMove()
+        {
+            _ship.Move(_ship.Front);
+            Assert.AreEqual(new Vector3(3, 3, 4), _ship.Pos);
+            _ship.Pitch(90);
+            Assert.AreEqual(Tuple.Create(Vector3.UnitY, -Vector3.UnitX), Tuple.Create(_ship.Front, _ship.Up));
+            _ship.Yaw(90);
+            Assert.AreEqual(Tuple.Create(-Vector3.UnitZ, -Vector3.UnitX), Tuple.Create(_ship.Front, _ship.Up));
+        }
+    }
+}
