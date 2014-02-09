34fa0d4926a8af7fee113a70a8e7971ff59863c6
Ville Nurmi <ville.v.nurmi@gmail.com>
2014-02-09 20:08:01 +0200

    Extracted state from Server.Service to Core.World

---
+++ Core/World.cs
@@ -0,0 +1,20 @@
+﻿using Core.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class World
+    {
+        private Dictionary<Guid, Station> _stations = new Dictionary<Guid, Station>();
+        private Dictionary<Guid, Ship> _ships = new Dictionary<Guid, Ship>();
+        private Dictionary<Guid, Inventory> _inventories = new Dictionary<Guid, Inventory>();
+
+        public Dictionary<Guid, Station> Stations { get { return _stations; } }
+        public Dictionary<Guid, Ship> Ships { get { return _ships; } }
+        public Dictionary<Guid, Inventory> Inventories { get { return _inventories; } }
+    }
+}
--- Server/Service.cs
+++ Server/Service.cs
@@ -11,9 +11,7 @@ namespace Server
 {
     internal class Service : IService
     {
-        private Dictionary<Guid, Station> _stations = new Dictionary<Guid, Station>();
-        private Dictionary<Guid, Ship> _ships = new Dictionary<Guid, Ship>();
-        private Dictionary<Guid, Inventory> _inventories = new Dictionary<Guid, Inventory>();
+        private World _world = new World();
 
         public Planet[] GetPlanets()
         {
@@ -22,24 +20,24 @@ namespace Server
 
         public Station[] GetStations()
         {
-            return _stations.Values.ToArray();
+            return _world.Stations.Values.ToArray();
         }
 
         public Ship[] GetShips()
         {
-            return _ships.Values.ToArray();
+            return _world.Ships.Values.ToArray();
         }
 
         public Ship GetShip(Guid id)
         {
             Ship ship;
-            _ships.TryGetValue(id, out ship);
+            _world.Ships.TryGetValue(id, out ship);
             return ship;
         }
 
         public void AddStation(Guid id, Vector3 pos)
         {
-            _stations[id] = new Station(id) { Pos = pos };
+            _world.Stations[id] = new Station(id) { Pos = pos };
         }
 
         public void UpdateShip(Guid id, Vector3 pos, Vector3 front, Vector3 up)
@@ -68,16 +66,16 @@ namespace Server
         private Ship GetOrAddShip(Guid id)
         {
             Ship ship;
-            if (!_ships.TryGetValue(id, out ship))
-                _ships.Add(id, ship = new Ship(id, Vector3.Zero, Vector3.UnitX, Vector3.UnitY));
+            if (!_world.Ships.TryGetValue(id, out ship))
+                _world.Ships.Add(id, ship = new Ship(id, Vector3.Zero, Vector3.UnitX, Vector3.UnitY));
             return ship;
         }
 
         private Inventory GetOrAddInventory(Guid id)
         {
             Inventory inventory;
-            if (!_inventories.TryGetValue(id, out inventory))
-                _inventories.Add(id, inventory = new Inventory(id));
+            if (!_world.Inventories.TryGetValue(id, out inventory))
+                _world.Inventories.Add(id, inventory = new Inventory(id));
             return inventory;
         }
     }
