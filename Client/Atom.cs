a5440d2c6ad1b3b26bda5cc78ac40d417b86bc7e
Ville Nurmi <ville.v.nurmi@gmail.com>
2014-03-10 22:53:24 +0200

    Added class Atom<T> to represent the identity of an object with synchronized access to its immutable state value

---
+++ Client/Atom.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Client
+{
+    internal class Atom<T>
+    {
+        public static implicit operator T(Atom<T> x) { return x.Value; }
+        public Atom(T value) { Value = value; }
+        public T Value { get; private set; }
+        public void Set(Func<T, T> f) { lock (Value) Value = f(Value); }
+    }
+}
